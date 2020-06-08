72e3a1872f2bbf5288831392ca74d9e8331c863c
Bradley Turek <UgmaDevelopment@gmail.com>
2020-06-07 19:01:04 -0600

    Yeah, I forgot to commit.
    Now we have a leaderboard; an ugly one, but we've got one.
    
    We're using a TreeSet for the moment to store the stuff.

--- complete/src/main/java/com/example/handlingformsubmission/AthleteSubmission.java
+++ complete/src/main/java/com/example/handlingformsubmission/AthleteSubmission.java
@@ -1,14 +1,17 @@
 package com.example.handlingformsubmission;
 
 import java.net.URI;
+import java.util.Objects;
 
 
 public class AthleteSubmission {
+
     private String name;
     private URI url;
     private Double mileage;
     private Double elevation;
     private URI avatarUrl;
+
 
     public String getName() {
         return name;
@@ -62,5 +65,34 @@ public class AthleteSubmission {
 
     public void setAvatarUrl(String avatarUrl) {
         this.avatarUrl = URI.create(avatarUrl);
+    }
+
+    @Override
+    public String toString() {
+        return "AthleteSubmission{" +
+                "name='" + name + '\'' +
+                ", url=" + url +
+                ", mileage=" + mileage +
+                ", elevation=" + elevation +
+                ", avatarUrl=" + avatarUrl +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AthleteSubmission that = (AthleteSubmission) o;
+        return name.equals(that.name) &&
+                url.equals(that.url);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, url);
+    }
+
+    public double getScore() {
+        return mileage + elevation/10;
     }
 }
--- complete/src/main/java/com/example/handlingformsubmission/ScravaSubmitController.java
+++ complete/src/main/java/com/example/handlingformsubmission/ScravaSubmitController.java
@@ -6,19 +6,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
 @Controller
 public class ScravaSubmitController {
 
-	@GetMapping("/")
-	public String greetingForm(Model model) {
-		model.addAttribute("athleteSubmission", new AthleteSubmission());
-		return "submit";
-	}
+    Set<AthleteSubmission> storage = new TreeSet<>(Comparator.comparingDouble(AthleteSubmission::getScore).reversed());
 
-	@PostMapping("/")
-	public String scravaSubmit(@ModelAttribute AthleteSubmission athleteSubmission, Model model) {
-		model.addAttribute("athleteSubmission", athleteSubmission);
-		return "result";
-	}
+    @GetMapping("/submit")
+    public String scravaSubmit(Model model) {
+        model.addAttribute("athleteSubmission", new AthleteSubmission());
+        return "submit";
+    }
+
+    @PostMapping("/submit")
+    public String scravaSubmissionResult(@ModelAttribute AthleteSubmission athleteSubmission, Model model) {
+        model.addAttribute("athleteSubmission", athleteSubmission);
+
+        storage.add(athleteSubmission);
+
+        return "result";
+    }
+
+    @GetMapping("/")
+    public String leaderBoard(Model model) {
+
+		System.out.println("Storage:");
+		storage.forEach(System.out::println);
+
+		model.addAttribute("athletes", storage);
+
+		System.out.println("Model:");
+		model.asMap().forEach((key, value) -> {
+			System.out.println("Key: " + key);
+			System.out.println("Value: " + value);
+		});
+		System.out.println();
+
+		return "leaderboard";
+    }
 
 }
